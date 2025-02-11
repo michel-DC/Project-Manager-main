d5d97fade127e14ff99e83601732cc040acda9cc
Michel <djoumessi.michelc@gmail.com>
2025-02-11 20:12:47 +0100

    Enhance Navbar and Projects page with new styles and functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { AiOutlineFundProjectionScreen } from "react-icons/ai";
 
 const Navbar: React.FC = () => {
   return (
     <nav className="w-full bg-black text-white p-4 border-b border-gray-800">
-      <h1 className="text-xl font-semibold">Project Manager</h1>
+      <div className="flex justify-center items-center w-=4xl">
+        <h1 className="text-4xl font-semibold text-center">
+          <AiOutlineFundProjectionScreen /> Project Manager
+        </h1>
+      </div>
     </nav>
   );
 };
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import ProjectCard from "../components/ProjectCard";
 import { sampleProjects } from "../data/sampleProjects";
+import { FaPlus } from "react-icons/fa";
 
+const title: string = "Mes Projets";
 const Projects: React.FC = () => {
   return (
     <div>
-      <h2 className="text-4xl font-semibold text-center">Mes Projets</h2>
+      <h2 className="text-4xl font-semibold text-center mb-8">{title}</h2>
+      <div className="flex justify-center items-center mt-4 p-2 bg-black rounded-lg border border-white w-2xl h-24 mx-auto mb-12">
+        <button
+          onClick={() => (window.location.href = "/new-project")}
+          className="flex px-4 py-2 text-black hover:bg-gray-300 cursor-pointer bg-white rounded-lg border border-black"
+        >
+          <FaPlus className="mr-1" />
+          Ajouter un nouveau projet
+        </button>
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
         {sampleProjects.map((project) => (
           <ProjectCard key={project.id} project={project} />
