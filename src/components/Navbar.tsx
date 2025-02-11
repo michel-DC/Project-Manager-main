1806a22aa7163ba9a47067d83edfa104c153b40b
Michel <djoumessi.michelc@gmail.com>
2025-02-11 22:50:09 +0100

    Enhance Navbar and Home page with improved styles and content

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,9 +4,10 @@ import { AiOutlineFundProjectionScreen } from "react-icons/ai";
 const Navbar: React.FC = () => {
   return (
     <nav className="w-full bg-black text-white p-4 border-b border-gray-800">
-      <div className="flex justify-center items-center w-=4xl">
-        <h1 className="text-4xl font-semibold text-center">
-          <AiOutlineFundProjectionScreen /> Project Manager
+      <div className="flex justify-center items-center mb-2 mt-1">
+        <h1 className="text-5xl font-semibold text-center w-120 flex items-center">
+          <AiOutlineFundProjectionScreen className="mr-5" />
+          Project Manager
         </h1>
       </div>
     </nav>
