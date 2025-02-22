6e3f8f844dc4aadf02ac57be3c7bc6b69c826640
Michel <djoumessi.michelc@gmail.com>
2025-02-22 10:39:00 +0100

    just a commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import AppRouter from "./router/AppRouter";
 import MainLayout from "./layouts/MainLayout";
 import { ProjectProvider } from "./context/ProjectContext";
+// import HeroSection from "./pages/HeroSection";
 
 const App: React.FC = () => {
   return (
+    // <HeroSection />
     <ProjectProvider>
       <MainLayout>
         <AppRouter />
